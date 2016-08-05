259e3e5c815132a551c2102436896f44dad5b573
MiroslavDragnev <mdragnev96@gmail.com>
2016-08-06 00:59:15 +0300

    Main.js updates (moving snake)

--- Snake/js/main.js
+++ Snake/js/main.js
@@ -1,136 +1,88 @@
 window.addEventListener('load', init);
 
-let pressingUp = false,
-	pressingDown = false,
-	pressingLeft = false,
-	pressingRight = false;
+//snake segment size
+const squareSize = 10;
+
+const snakeInitialSize = 3;
+
+//updating this when a new key is pressed
+let moveDelta = {};
+
+//the snake will be an array of objects
+//each object will have x and y properties
+let snake = [];
 
 function init(ev) {
 	document.addEventListener('keydown', PressingKey);
-	document.addEventListener('keyup', ReleasingKey);
+
+	setTimeout(CreateSnake, 100);
+	setTimeout(SnakeThinker, 500);
 }
 
 function PressingKey(ev) {
 	let btn = ev.code;
 
 	if(btn === "ArrowUp"){
-		UpdatePressedKeys(true, false, false, false);
-	}
-
-	if(btn === "ArrowDown"){
-		UpdatePressedKeys(false, true, false, false);
-	}
-
-	if(btn === "ArrowLeft"){
-		UpdatePressedKeys(false, false, true, false);
-	}
-
-	if(btn === "ArrowRight"){
-		UpdatePressedKeys(false, false, false, true);
-	}
-
-	//using this function in order to set all keys, except the last one pressed, to false
-	//in order to avoid a snake moving diagonally :D
-	function UpdatePressedKeys(up, down, left, right) {
-		pressingUp = up;
-		pressingDown = down;
-		pressingLeft = left;
-		pressingRight = right;
+		moveDelta = {x: 0, y: -squareSize};
+	} else if(btn === "ArrowDown"){
+		moveDelta = {x: 0, y: squareSize};
+	} else if(btn === "ArrowLeft"){
+		moveDelta = {x: -squareSize, y: 0};
+	} else if(btn === "ArrowRight"){
+		moveDelta = {x: squareSize, y: 0};
 	}
 }
 
-function ReleasingKey(ev) {
-	let btn = ev.code;
+function CreateSnake() {
+	let canvas = document.getElementById('gameCanvas'),
+		ctx = canvas.getContext('2d'),
+		middleX = canvas.width / 2,
+		middleY = canvas.height / 2,
+		i = 0;
 
-	if(btn === "ArrowUp"){
-		pressingUp = false;
+	//initialize snake (pointing down)
+	for(i = snakeInitialSize - 1; i >= 0; i -= 1) {
+		snake.push({x: middleX, y: middleY - i*squareSize});
 	}
 
-	if(btn === "ArrowDown"){
-		pressingDown = false;
-	}
+	//snake initial direction - down
+	moveDelta = {x: 0, y: squareSize};
+}
 
-	if(btn === "ArrowLeft"){
-		pressingLeft = false;
-	}
+function SnakeThinker() {
+	//make snake move
+	SnakeMove();
+	
+	//draw current snake
+	DrawSnake();
 
-	if(btn === "ArrowRight"){
-		pressingRight = false;
+	//why setTimeout instead of interval? because we'll be speeding up the snake as it grows
+	setTimeout(SnakeThinker, 100);
+}
+
+function DrawSnake() {
+	let canvas = document.getElementById('gameCanvas'),
+		ctx = canvas.getContext('2d'),
+		i = 0;
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = 'rgb(0, 0, 0)';
+	ctx.strokeStyle = 'rgb(255, 255, 255)';
+
+	for(i = 0; i < snake.length; i += 1) {
+		ctx.fillRect(snake[i].x, snake[i].y, squareSize, squareSize);
+		ctx.strokeRect(snake[i].x, snake[i].y, squareSize, squareSize);
 	}
 }
 
-$(document).ready(function() {
-	// get the canvas element
-	var canvas = $('#gameCanvas')[0];
-	var ctx = canvas.getContext('2d');
-	var width = $('#gameCanvas').width();
-	var height = $('#gameCanvas').height();
-	var i, length, snakePositionX, snakePositionY, tail, currentSnake;
-
-	// set the snake
-	var snakeArr;
-
-	// controller size of the snake
-	var cw = 30;
-
-	function snakeCreate() {
-		// initial empty array
-		snakeArr = [];
-
-		// initial length of the snake
-		length = 3;
-
-		// set startUp position of the snake
-		for (i = length - 1; i >= 0; i -= 1) {
-			snakeArr.push({
-				x: i,
-				y: 10
-			});
-		}
+function SnakeMove() {
+	let prevHeadPos = snake[snake.length-1],
+		nextHeadPos = {
+		x: prevHeadPos.x + moveDelta.x,
+		y: prevHeadPos.y + moveDelta.y
 	}
 
-	function snakePrint() {
-		// set the canvas style on each interval based on the canvas class
-		if (canvas.className === 'bgButtonChange') {
-			ctx.fillStyle = 'black';
-			ctx.strokeStyle = 'white';
-		} else {
-			ctx.fillStyle = 'white';
-			ctx.strokeStyle = 'black';
-		}
-
-		ctx.fillRect(0, 0, width, height);
-		ctx.strokeRect(0, 0, width, height);
-
-		// get the position of the first element of the snake - the head
-		snakePositionX = snakeArr[0].x;
-		snakePositionY = snakeArr[1].y;
-
-		// get the new position of the head
-		snakePositionX += 1;
-
-		// removes the last cell of the snake - the tail
-		tail = snakeArr.pop();
-
-		// set the position of the head
-		tail.x = snakePositionX;
-
-		// remove the tail and sets it up on first position
-		snakeArr.unshift(tail);
-
-		length = snakeArr.length;
-
-		for (i = 0; i < length; i += 1) {
-			currentSnake = snakeArr[i];
-			// set color of the body
-			ctx.fillStyle = 'green';
-			ctx.fillRect(currentSnake.x * cw, currentSnake.y * cw, cw, cw);
-			// set border for each part of the body
-			ctx.strokeStyle = 'white';
-			ctx.strokeRect(currentSnake.x * cw, currentSnake.y * cw, cw, cw);
-		}
-	}
-
-	snakeCreate();
-	game_loop = setInterval(snakePrint, 80);
-});
+	snake.push(nextHeadPos);
+	snake = snake.slice(1);
+}
